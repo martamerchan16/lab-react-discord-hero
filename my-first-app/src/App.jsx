bfd13145b0046acd7dab8b50d65c466caea18d9c
Marta Merchan <martamerchan16@gmail.com>
2024-07-23 20:26:11 +0200

    solved

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero/Hero'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ my-first-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <section className='Hero'>
+            <nav id='navBar'>
+                <img src="./src/assets/logo.png" alt="logo" />
+                <img src="./src/assets/menu-icon.png" alt="logo" />
+            </nav>
+            <hr />
+
+        </section>
+    )
+}
+
+export default Hero
+
